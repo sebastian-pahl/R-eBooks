bde542376057eeefc3b8461d9f9bbc7116ce6ea9
Sebastian Pahl <sebastianpahl@Sebastians-MacBook-Pro.local>
2021-08-02 21:34:50 +0200

    added sample output images

---
+++ R code/Strava_plotting.R
@@ -0,0 +1,92 @@
+
+
+#------------PLOT3D-------------------------------------------------------------
+
+#3D with coordinates on x and y axis, and elevation on z axis
+#need packages:
+#library("plot3D")
+#library("plot3Drgl") #also need this (but just once off): library("rJava")
+
+scatter3D(x = strava_data$latitude, y = strava_data$longitude, z = strava_data$elevation, 
+          colvar = pmax(150, pmin(300, strava_data$power)),
+          theta=15,  #viewing direction: increasing theta shifts image to the left 
+          phi=15,    #viewing direction: increasing phi gives more top view
+          bty="g",   #creates an empty perspective box
+          type="l",  #or "l" for lines; or "h" for histogram
+          lwd=5,
+          main= "3D Elevation Plot",
+          xlab = "Longitude", ylab = "Latitude", zlab = "Elevation", clab = "Power")
+#interactive version of plot
+plotrgl()
+
+
+#-----------------GGPLOT--------------------------------------------------
+
+
+#elevation ggplot
+elevation_gain = sum(pmax(0, strava_data$elevation[-1]-head(strava_data$elevation,-1)))
+ggplot(data = strava_data) +
+        geom_point(mapping = aes(x = hours, y = elevation, 
+                                colour = pmax(100, pmin(300, power))), 
+                                size=2) +
+        theme_bw() +
+        scale_x_continuous() +
+        labs(x = "Time in hours", y = "Elevation in meters", title = "Elevation Plot", 
+             subtitle = paste("Total elevation gain =", elevation_gain, "m"),
+             colour = "Power in watts"
+             ) +
+        #scale_color_gradient2(low = "yellow", high = "red")
+        scale_colour_continuous(type = "viridis")
+
+#coordinates plotted on google maps
+library("ggmap")
+register_google(key = "<key>") #generate own Maps API on Google Cloud Platform
+StravaMap <- get_googlemap(center = c(mean(strava_data$longitude), mean(strava_data$latitude)),
+                           zoom =11)
+ggmap(StravaMap) +
+        geom_point(data=strava_data, aes(longitude, latitude, colour=elevation), 
+                   size = 1) +
+        scale_color_gradient2(low = "yellow", high = "black", mid = "red", midpoint=500)
+#ggplotly(y)  #can use ggplotly to create interactive plot in Viewer, but quite laggy
+
+library("plotly")
+x <- plot_ly(data = strava_data, 
+        x = strava_data$latitude, y = strava_data$longitude, z = strava_data$elevation, 
+        color = pmax(100, pmin(300, strava_data$power)),
+        #colorscale = "Viridis", colorbar = list(title = "power"),
+        type="scatter3d", size=3,
+        marker=list(
+                colorbar=list(title='Power'),
+                colorscale='Viridis',
+                reversescale = T
+        )
+)
+
+layout(x, title = "Strava interactive plotly",
+       scene = list(
+               xaxis = list(title = "latitude"),
+               yaxis = list(title = "longitude"),
+               zaxis = list(title = "elevation")
+               )
+       )
+
+
+#-----------------MPG DATA--------------------------------------
+
+
+world <- map_data("world")
+countries <- unique(world$region)
+namibia = world[(world$region == "Namibia"),]
+germany = world[(world$region == "Germany"),]
+head(germany)
+head(strava_data)
+?openmap
+
+#ggplot(data = <DATA>) + 
+#        <GEOM_FUNCTION>(
+#                mapping = aes(<MAPPINGS>),
+#                stat = <STAT>, 
+#                position = <POSITION>
+#        ) +
+#        <COORDINATE_FUNCTION> +
+#        <FACET_FUNCTION>
---
+++ R code/Strava_testing.R
@@ -0,0 +1,71 @@
+
+
+#-----------------------TO DO LIST---------------------------
+
+
+#plot vs meters OR time
+#first check if power, hr and cadence data is available
+#download from either original data or gpx
+#add power zones and hr zones
+#power curve and 25w distribution
+#give summary stats: total time, total elevation, avg power, avg speed, avg hr, avg cadence
+#manual inputs like weight, max HR, FTP
+#interesting stats: avg stats (gradient, hr, power) for given distance/time
+#calories
+
+
+#------------------------TESTING-----------------------------
+
+
+data.gpx <- "Morning_Ride_Kandel.gpx"
+data_table <- read.csv(data.gpx, strip.white = TRUE, skip=1,col.names = c("column1"))
+data_character <- as.character(data_table[,1])
+
+find_time <- grep("<time>", data_character, value=TRUE)
+activity_date <- strptime(find_time, "<time>%Y-%m-%dT%H:%M:%SZ</time>")
+hours <- difftime(activity_date[-1], activity_date[1])/60/60
+
+find_elevation <- grep("<ele>", data_character, value=TRUE)
+elevation <- as.numeric(gsub("</?ele>","",find_elevation))
+
+find_coordinates <- grep("<trkpt lat", data_character, value=TRUE)
+latitude <- as.numeric(substr(find_coordinates,12,21))
+longitude <- as.numeric(substr(find_coordinates,27,35))
+
+#extensions
+data_string <- toString(data_table)
+extensions <- c(str_split(data_string, "/extensions", simplify=TRUE))
+
+#power
+power_function <- function(x) {
+        extract_power <- str_extract(x, "<power>.*</power>")
+        power <- str_extract(extract_power, "\\d+")
+        power
+}
+power <- head(na.locf(as.numeric(lapply(extensions, power_function))),-1)
+
+#pulse
+pulse_function <- function(x) {
+        extract_pulse <- str_extract(x, "<gpxtpx:hr>.*</gpxtpx:hr>")
+        pulse <- str_extract(extract_pulse, "\\d+")
+        pulse
+}
+pulse <- head(na.locf(as.numeric(lapply(extensions, pulse_function))),-1)
+
+#cadence
+cadence_function <- function(x) {
+        extract_cadence <- str_extract(x, "<gpxtpx:cad>.*</gpxtpx:cad>")
+        cadence <- str_extract(extract_cadence, "\\d+")
+        cadence
+}
+cadence <- head(na.locf(as.numeric(lapply(extensions, cadence_function))),-1)
+
+#temperature
+temperature_function <- function(x) {
+        extract_temperature <- str_extract(x, "<gpxtpx:atemp>.*</gpxtpx:atemp>")
+        temperature <- str_extract(extract_temperature, "\\d+")
+        temperature
+}
+temperature <- head(na.locf(as.numeric(lapply(extensions, temperature_function))),-1)
+
+strava_data <- strava_table_creator(data.gpx)
